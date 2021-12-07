4056c86fc46d8eed74209bbcefb2bfcfbd88f4fd
still-not-telling <JR212384@reigate.ac.uk>
2021-12-07 11:40:31 +0000

    removing books now works

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     private static File bookshelf = new File("bookshelf.txt");
 
+
     public static void createShelf() {
+
         try {
             if (bookshelf.createNewFile()) {
                 System.out.println("bookshelf successfully created");
@@ -21,6 +21,34 @@ public class Main {
         }
     }
 
+    public static int genreToKey(String genre) {
+        switch (genre) {
+
+            case ("thriller"):
+                return 1;
+
+            case ("horror"):
+                return 2;
+
+            case ("romance"):
+                return 3;
+
+            case ("comedy"):
+                return 4;
+
+            case ("graphic novel"):
+                return 5;
+
+            case ("non fiction"):
+                return 6;
+
+            default:
+                return 7;
+        }
+
+    }
+
+
     public static String getInput(String prompt) {
         System.out.println(prompt);
         Scanner input = new Scanner(System.in);
@@ -28,18 +56,20 @@ public class Main {
     }
 
     public static void bookRegister() {
-
+        String delimiter = "~";
         Scanner input = new Scanner(System.in);
         String title = getInput("please enter the title of the book");
         System.out.println("please enter the book ISBN");
         String ISBN = input.next();
         String author = getInput("please enter the author of the book");
+        String genre = getInput("please enter the genre of the book, your choices are: thriller , horror, romance, comedy , graphic novel,non fiction, other");
         System.out.println("adding book to database");
-        String genre = getInput("please enter the genre of the book");
         try {
-            FileWriter librarian = new FileWriter(bookshelf.getName(), true);
-            librarian.write(title + "~" + ISBN + "~" + author + "~" + genre + "\n");
+
+            FileWriter librarian = new FileWriter(bookshelf, true);
+            librarian.write(title + delimiter + ISBN + delimiter + author + delimiter + genreToKey(genre) + "\n");
             librarian.close();
+
             System.out.println("book successfully added");
         } catch (IOException e) {
             System.out.println("error, unable to add book");
@@ -48,16 +78,47 @@ public class Main {
     }
 
     public static void bookCheck() {
+
         try {
             Scanner fileReader = new Scanner(bookshelf);
-        while (fileReader.hasNextLine()) {
-            String bookInfo = fileReader.nextLine();
-            System.out.println(bookInfo);
+
+            if (bookshelf.getName().length() == 0) {
+                System.out.println("there are no books currently registered");
+                fileReader.close();
+            } else {
+                while (fileReader.hasNextLine()) {
+                    String bookInfo = fileReader.nextLine() + "\n";
+                    System.out.println(bookInfo);
+                }
+                fileReader.close();
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("an error occured");
         }
-        fileReader.close();
-        } catch (FileNotFoundException e){
-            System.out.println("an error occurred");
-            e.printStackTrace();
+    }
+
+    public static void bookRemove() {
+        ArrayList<String> temp = new ArrayList<>();
+        try {
+            bookCheck();
+            String removeChoice = getInput("which book would you like to remove?");
+            Scanner librarian = new Scanner(bookshelf);
+            while (librarian.hasNextLine()) {
+                String line = librarian.nextLine();
+                if (line.contains(removeChoice)) {
+                } else {
+                    temp.add(line);
+                }
+            }
+            FileWriter remover = new FileWriter(bookshelf, false);
+            for (int i = 0; i < temp.size(); i++) {
+                remover.write(temp.get(i) + "\n");
+                remover.close();
+            }
+            remover.close();
+
+        } catch (IOException e) {
+            System.out.println("error , file could not be found" + e);
         }
     }
 
@@ -65,7 +126,7 @@ public class Main {
         System.out.println("welcome to the library");
         boolean leave = false;
         while (!leave) {
-            System.out.println(" would you like to register a book, or check what books are available?");
+            System.out.println(" would you like to register a book,remove a book, or check what books are available?");
             Scanner input = new Scanner(System.in);
             switch (input.next()) {
                 case ("register"):
@@ -77,6 +138,9 @@ public class Main {
                 case ("leave"):
                     System.out.println("goodbye, come again soon");
                     leave = true;
+                    break;
+                case ("remove"):
+                    bookRemove();
                     break;
             }
         }
